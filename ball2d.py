67f63242fb64ee7d588bdec7299f3061d153e74e
Ryan Sandzimier <rsandz@mit.edu>
2020-04-17 00:20:18 -0700

    Objects now have access to other object's states

--- ball2d.py
+++ ball2d.py
@@ -3,6 +3,7 @@ import numpy as np
 from pydrake.systems.framework import BasicVector_, LeafSystem_, PortDataType
 from pydrake.systems.pyplot_visualizer import PyPlotVisualizer
 from pydrake.systems.scalar_conversion import TemplateSystem
+from pydrake.symbolic import Expression
 
 # Note: In order to use the Python system with drake's autodiff features, we
 # have to add a little "TemplateSystem" boilerplate (for now).  For details,
@@ -28,11 +29,28 @@ def Ball2D_(T):
             self.DeclareContinuousState(2, 2, 0)
 
             # Quadrotor states as inputs
-            for i in range(self.n_quadrotors):
-                self.DeclareVectorInputPort("quad_"+str(i), BasicVector_[T](6))
+            max_quads = 5
+            # NOTE: For some reason, using a loop to call self.DeclareVectorInputPort n_quadrotors number
+            # of times leads to a segmentation fault. As a dirty fix, declare some hard-coded maximum number
+            # of vector inputs instead of using a for loop
+            assert n_quadrotors <= max_quads, "Max number of quadrotors exceeded in Ball2D."
+            self.DeclareVectorInputPort("quad_0", BasicVector_[T](6))
+            self.DeclareVectorInputPort("quad_1", BasicVector_[T](6))
+            self.DeclareVectorInputPort("quad_2", BasicVector_[T](6))
+            self.DeclareVectorInputPort("quad_3", BasicVector_[T](6))
+            self.DeclareVectorInputPort("quad_4", BasicVector_[T](6))
+
             # Other ball states as inputs
-            for i in range(self.n_balls):
-                self.DeclareVectorInputPort("ball_"+str(i), BasicVector_[T](4))
+            max_balls = 5
+            # NOTE: For some reason, using a loop to call self.DeclareVectorInputPort n_balls number
+            # of times leads to a segmentation fault. As a dirty fix, declare some hard-coded maximum number
+            # of vector inputs instead of using a for loop
+            assert n_balls <= max_balls, "Max number of balls exceeded in Ball2D."
+            self.DeclareVectorInputPort("ball_0", BasicVector_[T](4))
+            self.DeclareVectorInputPort("ball_1", BasicVector_[T](4))
+            self.DeclareVectorInputPort("ball_2", BasicVector_[T](4))
+            self.DeclareVectorInputPort("ball_3", BasicVector_[T](4))
+            self.DeclareVectorInputPort("ball_4", BasicVector_[T](4))
 
             self.radius = 0.1
             self.mass = 1.0
@@ -49,6 +67,7 @@ def Ball2D_(T):
             x = context.get_continuous_state_vector().CopyToVector()
             q = x[:2]
             qdot = x[2:]
+
             qddot = np.array([0,-self.gravity])
             derivatives.get_mutable_vector().SetFromVector(
                 np.concatenate((qdot, qddot)))
