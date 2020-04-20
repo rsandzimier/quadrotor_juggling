0cc58888e058d57431e175205c93f31fb3557a56
FilipposSot <filippos.sotiropoulos@gmail.com>
2020-04-20 17:26:59 +0300

    trying to modify each subcontext separately

--- main.py
+++ main.py
@@ -28,8 +28,10 @@ builder = DiagramBuilder()
 # Setup quadrotor plants and controllers
 quadrotor_plants = []
 quadrotor_controllers = []
-for _ in range(n_quadrotors):
-    plant = builder.AddSystem(Quadrotor2D(n_quadrotors=n_quadrotors-1, n_balls=n_balls))
+for i in range(n_quadrotors):
+    new_quad = Quadrotor2D(n_quadrotors=n_quadrotors-1, n_balls=n_balls)
+    new_quad.set_name('quadcopter_' + str(i))
+    plant = builder.AddSystem(new_quad)
     quadrotor_plants.append(plant)
     controller = QuadrotorLQR(plant, n_quadrotors-1, n_balls)
     quadrotor_controllers.append(builder.AddSystem(controller))
@@ -38,8 +40,10 @@ for _ in range(n_quadrotors):
 
 # Setup ball plants
 ball_plants = []
-for _ in range(n_balls):
-    plant = builder.AddSystem(Ball2D(n_quadrotors=n_quadrotors, n_balls=n_balls-1))
+for i in range(n_balls):
+    new_ball = Ball2D(n_quadrotors=n_quadrotors, n_balls=n_balls-1)
+    new_ball.set_name('ball_' + str(i))
+    plant = builder.AddSystem(new_ball)
     ball_plants.append(plant)
 
 # Connect all plants so that each object (quadrotor or ball) has access to all other object states as inputs
@@ -60,14 +64,22 @@ for i in range(n_balls):
         k = j if j < i else j-1
         builder.Connect(ball_plants[j].get_output_port(0), ball_plants[i].GetInputPort('ball_'+str(k)))
 
-# Setup visualization
-visualizer = builder.AddSystem(Visualizer(n_quadrotors=n_quadrotors, n_balls=n_balls))
-for i in range(n_quadrotors):
-    builder.Connect(quadrotor_plants[i].get_output_port(0), visualizer.get_input_port(i))
-for i in range(n_balls):
-    builder.Connect(ball_plants[i].get_output_port(0), visualizer.get_input_port(n_quadrotors + i))
+# # Setup visualization
+# visualizer = builder.AddSystem(Visualizer(n_quadrotors=n_quadrotors, n_balls=n_balls))
+# for i in range(n_quadrotors):
+#     builder.Connect(quadrotor_plants[i].get_output_port(0), visualizer.get_input_port(i))
+# for i in range(n_balls):
+#     builder.Connect(ball_plants[i].get_output_port(0), visualizer.get_input_port(n_quadrotors + i))
 
 diagram = builder.Build()
+context = diagram.CreateDefaultContext()
+print(diagram.EvalTimeDerivatives(context).get_vector())
+# EvalTimeDerivatives
+
+# def TemplateResidualFunction(vars):
+#     assert vars.size == 11*n_quadrotors + 6*n_balls
+
+# context.SetContinuousState(np.array([0.0, -1.5 , 0.0, 0.]))
 
 # plt.figure(figsize=(20, 10))
 # plot_system_graphviz(diagram)
@@ -78,6 +90,7 @@ integrator = simulator.get_mutable_integrator()
 integrator.set_maximum_step_size(0.01) # Reduce the max step size so that we can always detect collisions
 context = simulator.get_mutable_context()
 
+##############################################3
 # state_init = np.array([-1.,1.,1.,0.,1.,1.,-1.,0.])
 # state_init = np.random.randn(n_quadrotors*6 + n_balls*4,)
 # state_init = np.array([0.0 ,1.,0.,0., 0.0 ,1.3,0.,0.,0.,1.8,0.0,-0.8])
